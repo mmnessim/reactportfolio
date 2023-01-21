ce0a53e55a3e71cb2054dfcf6d706b3ee70cc0e8
Mounir Nessim <mmnessim@gmail.comgit config --global user.name Mounir>
2023-01-21 09:24:29 -0500

    under construction and style changes

--- src/components/nav/navigation.js
+++ src/components/nav/navigation.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import './navigation.css'
 import { AboutMe } from "../pages/aboutme";
 import { Media } from "../pages/media";
+import { Landing } from "../pages/landing";
 import { Arrangements } from "../pages/arrangements";
 
 export function NavigationMenu(props) {
@@ -13,11 +14,15 @@ export function NavigationMenu(props) {
     } else if (name === 'Media') {
         pageContent = <Media />
     } else if (name === 'Arrangements') {
-        pageContent = <Arrangements />}
+        pageContent = <Arrangements />
+    } else if (name === 'Landing') {
+        pageContent = <Landing />
+    }
 
     return (
         <div>
         <div className='nav'>
+            <span onClick={() => setName('Landing')}>Home</span>
             <span onClick={() => setName('About Me')}>About Me</span>
             <span onClick={() => setName('Media')}>Media</span>
             <span onClick={() => setName('Arrangements')}>Arrangements</span>
---
+++ src/components/pages/landing.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export function Landing() {
+    return(
+        <div className="content">
+            <h2>Under Construction!!!</h2>
+            <h4>January 20, 2023</h4>
+            <p>Welcome to an in-development web portfolio written in React.js. I intend for this website to be an place to display and test some of the things that I've been learning. At the moment, I am just filling out the basic structure of the page, such as the different sections, content, and functionality. Don't be surprised if things look wonky, or if something is broken. Almost everything at this point is a placeholder, including the color scheme and styling.</p>
+            <p>All that being said, welcome and feel free to poke around. I'm hoping to make this a more inviting page soon!</p>
+        </div>
+    )
+}
