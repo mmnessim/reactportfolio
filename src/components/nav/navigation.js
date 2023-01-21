414ad754719730fff953da2d886cadbba307fece
Mounir Nessim <mmnessim@gmail.comgit config --global user.name Mounir>
2023-01-21 17:27:55 -0500

    page was not working, now fixed

--- src/components/nav/navigation.js
+++ src/components/nav/navigation.js
@@ -4,7 +4,7 @@ import { AboutMe } from "../pages/aboutme";
 import { Media } from "../pages/media";
 import { Landing } from "../pages/landing";
 import { Arrangements } from "../pages/arrangements";
-//import { Interactive } from "../pages/interactive";
+
 
 export function NavigationMenu(props) {
     const [name, setName] = useState('Landing');
@@ -18,9 +18,7 @@ export function NavigationMenu(props) {
         pageContent = <Arrangements />
     } else if (name === 'Landing') {
         pageContent = <Landing />
-    } else /*if (name === 'Interactive') {
-        pageContent = <Interactive />
-    }*/
+    } 
 
     return (
         <div>
@@ -29,7 +27,6 @@ export function NavigationMenu(props) {
             <span onClick={() => setName('About Me')}>About Me</span>
             <span onClick={() => setName('Media')}>Media</span>
             <span onClick={() => setName('Arrangements')}>Arrangements</span>
-            {/*<span onClick={() => setName('Interactive')}>Interactive</span>*/}
         </div>
         {pageContent}
         </div>
--- src/components/pages/media.js
+++ src/components/pages/media.js
@@ -4,7 +4,9 @@ export function Media() {
     return (
         <div className="content">
             <h3>Media</h3> 
-            <p>YouTube links probably</p>
+            
+            
+            
         </div>
     )
 }
