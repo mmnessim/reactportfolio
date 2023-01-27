203f60c1d18cf6ac689c700170f52580b91fe83d
Mounir Nessim <mmnessim@gmail.comgit config --global user.name Mounir>
2023-01-27 06:09:33 -0500

    pdf test

--- src/components/arrangements/arrangements.js
+++ src/components/arrangements/arrangements.js
@@ -4,7 +4,7 @@ export function Arrangements() {
     return (
         <div className="content">
             <h3>Arrangements and Compositions</h3>
-            <p>Under construction</p>
+            <p>Under construction <a href="../../../public/pdfs/score.pdf" target="_blank">Hello</a> </p>
         </div>
     )}
     
