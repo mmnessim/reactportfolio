aa92dd21de1354bfd717288986cabf50e4be75d8
Mounir Nessim <mmnessim@gmail.comgit config --global user.name Mounir>
2023-01-27 06:25:53 -0500

    fixed deploy

--- src/components/arrangements/arrangements.js
+++ src/components/arrangements/arrangements.js
@@ -5,7 +5,7 @@ export function Arrangements() {
     return (
         <div className="content">
             <h3>Arrangements and Compositions</h3>
-            <p>Under construction. Soon this area will contain links to viola ensemble arrangements I made during the pandemic. Until then, enjoy just one link. <a href={pdf} target="_blank">Adoration by Florence Price</a> </p>
+            <p>Under construction. Soon this area will contain links to viola ensemble arrangements I made during the pandemic. Until then, enjoy just one link. <a href={pdf} target="_blank" rel="noreferrer">Adoration by Florence Price</a> </p>
         </div>
     )}
     
