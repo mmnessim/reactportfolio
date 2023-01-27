a6eda6ea9489660f5d75d05e883094407d33537c
Mounir Nessim <mmnessim@gmail.comgit config --global user.name Mounir>
2023-01-27 06:22:43 -0500

    Fixed pdf, need to add the rest of the pdf files

--- src/components/arrangements/arrangements.js
+++ src/components/arrangements/arrangements.js
@@ -1,10 +1,11 @@
 import React from "react";
+import pdf from './pdfs/score.pdf';
 
 export function Arrangements() {
     return (
         <div className="content">
             <h3>Arrangements and Compositions</h3>
-            <p>Under construction <a href="../../../public/pdfs/score.pdf" target="_blank">Hello</a> </p>
+            <p>Under construction. Soon this area will contain links to viola ensemble arrangements I made during the pandemic. Until then, enjoy just one link. <a href={pdf} target="_blank">Adoration by Florence Price</a> </p>
         </div>
     )}
     
--- src/components/pages/landing.js
+++ src/components/pages/landing.js
@@ -4,6 +4,8 @@ export function Landing() {
     return(
         <div className="content">
             <h2>Under Construction!!!</h2>
+            <h4>January 27, 2023</h4>
+            <p>The most recent update to the site is that I've figured out how to link the pdfs to my arrangements. This would be very simple in a static site, but it turns out it's a bit different in React. Now, it's just a matter of finding the time to implement it for the rest of the pdfs. Up next, I am considering changing the styling of the page to something more appealing. Everything is a bit too green and plain right now. I could also use a few more pictures probably. I would also like to add an area for interactive elements to practice a few things.</p>
             <h4>January 20, 2023</h4>
             <p>Welcome to an in-development web portfolio written in React.js. I intend for this website to be an place to display and test some of the things that I've been learning. At the moment, I am just filling out the basic structure of the page, such as the different sections, content, and functionality. Don't be surprised if things look wonky, or if something is broken. Almost everything at this point is a placeholder, including the color scheme and styling.</p>
             <p>All that being said, welcome and feel free to poke around. I'm hoping to make this a more inviting page soon!</p>
