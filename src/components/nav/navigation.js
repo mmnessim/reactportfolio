9ef030f541514d24a7b9bb56bcb41de4d2aaf3ac
mmnessim <119817779+mmnessim@users.noreply.github.com>
2023-03-24 15:24:41 -0400

    Add files via upload

--- src/components/nav/navigation.js
+++ src/components/nav/navigation.js
@@ -1,41 +1,18 @@
-import React, { useState } from "react";
-import './navigation.css'
-import { AboutMe } from "../pages/aboutme";
-import { Media } from "../pages/media";
-import { Landing } from "../pages/landing/landing";
-import { Container } from "../pages/interactive/container";
-import { Data } from "../pages/data/data";
-import { Footer } from "../footer/footer";
-
+import React from "react";
+import { Link } from "react-router-dom";
+import './navigation.css';
 
 
 export function NavigationMenu(props) {
-    const [name, setName] = useState('Data');
-    let pageContent = '';
-    
-    if (name === 'About Me') {
-        pageContent = <AboutMe />
-    } else if (name === 'Media') {
-        pageContent = <Media />
-    } else if (name === 'Landing') {
-        pageContent = <Landing />
-    } else if (name === 'Interactive') {
-        pageContent = <Container />
-    } else if (name === 'Data') {
-        pageContent = <Data />
-    } 
-
     return (
         <div>
-        <div className='nav'>
-            <span onClick={() => setName('Landing')} className='btn  col-sm navlink'>Home</span>
-            <span onClick={() => setName('About Me')} className='btn  col-sm navlink'>About Me</span>
-            <span onClick={() => setName('Media')} className='btn  col-sm navlink'>Media</span>
-            <span onClick={() => setName('Interactive')} className='btn  col-sm navlink'>Interactive</span>
-            <span onClick={() => setName('Data')} className='btn  col-sm navlink'>Data</span>
-        </div>
-        {pageContent}
-        <Footer />
+            <div className='nav'>
+                <Link to={'/'} className='btn col-sm navlink'>Home</Link>
+                <Link to={'/about'} className='btn col-sm navlink'>About Me</Link>
+                <Link to={'/media'} className='btn col-sm navlink'>Media</Link>
+                <Link to={'/interactive'} className='btn col-sm navlink'>Interactive</Link>
+                <Link to={'/data'} className='btn col-sm navlink'>Data</Link>
+            </div>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -3,13 +3,49 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { Header } from './components/header/header';
+import { Landing } from './components/pages/landing/landing';
+import { AboutMe } from './components/pages/aboutme';
+import { Media } from './components/pages/media';
+import { Interactive } from './components/pages/interactive/interactive';
+import { Data } from './components/pages/data/data';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Header />,
+    children: [
+      {
+        path: '/',
+        element: <Landing />
+      },
+      {
+        path: '/about',
+        element: <AboutMe />
+      },
+      {
+        path: '/media',
+        element: <Media />
+      },
+      {
+        path: '/interactive',
+        element: <Interactive />
+      },
+      {
+        path: '/data',
+        element: <Data />
+      }
+    ],
+  },
+]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <div id='body'>
-    <Header />
-  </div>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
