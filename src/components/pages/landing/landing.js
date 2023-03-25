81651d5c7d1bb75dd241af38e7e1ba92b53f45dc
Mounir Nessim <mmnessim@gmail.comgit config --global user.name Mounir>
2023-03-25 06:34:20 -0400

    change date

--- src/components/pages/landing/landing.js
+++ src/components/pages/landing/landing.js
@@ -6,7 +6,7 @@ export function Landing() {
         <div>
             <div className="content">
                 <h2>Updates</h2>
-                <h4>March 24, 2023</h4>
+                <h4>March 25, 2023</h4>
                 <p>The most recent updates to the site have been somewhat quiet. As I laid out in the last update, I wanted to work on the styling some. I ended learning the basics of using Bootstrap and implementing it to tweak some of the style of the page. It's probably most noticeable on the navigation bar, though it's quite minor. At some point, I would like to do a full overhaul of the styling to fully incorporate Boostrap.</p>
                 <p>Another small, but important change has been the implentation of React Router. This whole site has been created using the React.js frontend library. The advantage of React is that you are able to have your site reload only the parts of a page that change, instead of reloading the entire page each time there is a change (like a more traditional site). One side effect of this is that the URL does not change when you navigate to different parts of the site. Instead of actually navigating to a new page, the site simply loads different content on the page. In order to create the effect of navigation, I wrote some cumbersome code to make the navigation bar work. Fortunately, there exists another library called React Router that handles this issue.</p>
                 <p>Long story short, you will now notice that the URL does change as you navigate around the site. This also allows me to link directly to a particlar section of the site.</p>
